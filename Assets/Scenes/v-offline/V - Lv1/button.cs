26106b5d340bf8396ae141176bfc944a4cfb741b
VencentV <104806343+VencentV@users.noreply.github.com>
2023-11-05 20:16:06 -0800

    Button Script
    Simple button script added

--- Assets/Scenes/v-offline/V - Lv1/button.cs
+++ Assets/Scenes/v-offline/V - Lv1/button.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 public class button : MonoBehaviour
 {
     // This method is called when the GameObject enters a trigger collider.
+    private bool hasBeenTriggered = false;
+
+    public float moveDistance = 0.2f;
+    public LayerMask triggerLayer;
+    public bool buttonTriggered;
+
+
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the entering object has a specific tag (optional).
-        if (other.gameObject.layer == LayerMask.NameToLayer("FoundryGrabbable"))
+        //Check if the entering object is on the "TriggeringLayer"
+        if (!hasBeenTriggered && ((1 << other.gameObject.layer) & triggerLayer) != 0)
         {
-            // Destroy the object this script is attached to.
-            Destroy(gameObject);
+            hasBeenTriggered = true; // Set the flag to true to indicate that it has been triggered
         }
+        print("triggered");
+
+        GetComponent<Collider>().enabled = false;
+        transform.position += Vector3.down * moveDistance;
+
+        // players need to have BOTH buttons pressed to trigger the sliding door
+        buttonTriggered = true;
     }
+
+    private void Update()
+    {
+        
+    }
+
 }
