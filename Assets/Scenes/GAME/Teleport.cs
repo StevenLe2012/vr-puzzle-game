24db9eae840da62ab1b73b89aa1c880713baad23
VencentV <104806343+VencentV@users.noreply.github.com>
2023-11-09 00:23:33 -0800

    yes
    yes

--- Assets/Scenes/GAME/Teleport.cs
+++ Assets/Scenes/GAME/Teleport.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Teleport : MonoBehaviour
 {
     public LayerMask playerLayer; // Set the Player layer in the Unity Inspector.
+
     public GameObject teleportDestination; // Drag and drop the TeleportDestination GameObject here in the Unity Inspector.
 
     private void OnTriggerEnter(Collider other)
     {
         if (((1 << other.gameObject.layer) & playerLayer) != 0)
         {
-            print("detected player");
             other.transform.position = teleportDestination.transform.position;
         }
     }
